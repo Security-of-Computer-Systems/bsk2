fce4edd85e0cf8e5a1393dfbc574070dccbc63c1
Nirane <wittbrodtdawid@wp.pl>
2021-06-20 17:27:17 +0200

    Serving pages and redirections working

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
-function loadFilms()
+function getFilms()
 {
     const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "localhost:5000/films", true);
-    xhttp.send();
 
-    console.log(xhttp.responseText);
+    xhttp.open("GET", "https://localhost:5000/films", true);
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+    
+        if (this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+            // we get the returned data
+        }
+        // end of state change: it can be after some time (async)
+    };
+
+    xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify({
+        username: "123",
+        password: "123"
+    }));
+
+    
 
     table = document.getElementById("filmsTable");
+
+    //for
     var row = table.insertRow(-1);
 
     var name = row.insertCell(0);
@@ -22,3 +42,84 @@ function loadFilms()
     owner.innerHTML = "NEW CELL2";
 }
 
+async function log_in()
+{
+    var login = document.getElementById("login").value;
+    var password = await hash(document.getElementById("password").value)
+    
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://localhost:5000/login", true);
+    xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+    
+        if (this.status == 302) {
+            console.log(this.responseURL)
+            // we get the returned data
+        }
+    
+        // end of state change: it can be after some time (async)
+    };
+
+    xhttp.send(JSON.stringify({
+        username: login,
+        password: password
+    }));
+
+    
+}
+
+async function hash(password)
+{
+    const msgUint8 = new TextEncoder().encode(password);                           // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-512', msgUint8);           // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+    return hashHex;
+}
+
+async function register()
+{
+    
+    var login = document.getElementById("login").value;
+    var password = await hash(document.getElementById("password").value)
+    
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://localhost:5000/users", true);
+    xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+    
+        if (this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            console.log(data)
+            // we get the returned data
+        }
+    
+        // end of state change: it can be after some time (async)
+    };
+
+    xhttp.send(JSON.stringify({
+        username: login,
+        password: password
+    }));
+}
+
+function set_film()
+{
+
+}
+
+function set_permission()
+{
+
+}
+
+function logout()
+{
+
+}
--- server.py
+++ server.py
@@ -1,31 +1,92 @@
+import os
+import random
+import string
+
+import flask
 import psycopg2
 import json
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask_cors import CORS
 
-app = Flask(__name__)
+address = "https://localhost:5000/"
+
+app = Flask(__name__, template_folder='html')
+CORS(app)
+cors = CORS(app, resource={
+    r"/*":{
+        "origins":"*"
+    }
+})
 app.config["DEBUG"] = True
+app.secret_key = os.urandom(24)
 
 def init_database(cur):
     cur.execute("""create table Films (
-    	id SERIAL ,
-    	title varchar(30),
-    	time integer,
+    	id SERIAL not null,
+    	title varchar(30) not null,
+    	time integer not null,
     	PRIMARY KEY (id)
     	)""")
     cur.execute("""create table Users (
-        	username varchar(20) primary key,
-        	password varchar(20)
+        	username varchar(20) primary key not null,
+        	password varchar(128) not null,
+        	sessionID varchar(256)
         	)""")
     cur.execute("""create table Permissions (
-        FK_Film integer, 
-	    FK_User varchar(20), 
-	    read boolean,
-	    write boolean,
-	    ownership boolean,
+        FK_Film integer not null, 
+	    FK_User varchar(20) not null, 
+	    read boolean not null,
+	    write boolean not null,
+	    ownership boolean not null,
 	    Primary KEY (FK_Film, FK_User),
 	    FOREIGN KEY (FK_Film) references Films (id) ON DELETE CASCADE,
 	    FOREIGN KEY (FK_User) references Users (username) ON DELETE CASCADE
         )""")
+    conn.commit()
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/show-films')
+def serve_films():
+    return flask.make_response(render_template('showFilms.html'))
+
+@app.route('/delete-film')
+def serve_delete_film():
+    return flask.make_response(render_template('deleteFilm.html'))
+
+@app.route('/add-film')
+def serve_add_film():
+    return flask.make_response(render_template('addFilm.html'))
+
+@app.route('/login', methods=['GET','POST'])
+def serve_login():
+    if request.method=="POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        sessionId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(256))
+
+        return flask.redirect(flask.url_for('serve_account'))
+    else:
+        return flask.make_response(render_template('login.html'))
+
+@app.route('/register')
+def serve_register():
+    return flask.make_response(render_template('register.html'))
+
+@app.route('/account')
+def serve_account():
+    return flask.make_response(render_template('menu.html'))
+
+@app.route('/permissions')
+def serve_permissions():
+    return flask.make_response(render_template('accessManipulation.html'))
 
 def authorize(username, password):
     cur.execute("SELECT password FROM Users WHERE username = %s;", (username,))
@@ -39,7 +100,6 @@ def authorize(username, password):
 def register():
     username = request.json['username']
     password = request.json['password']
-
     try:
         cur.execute("INSERT INTO Users VALUES(%s, %s)", (username, password))
         conn.commit()
@@ -48,23 +108,50 @@ def register():
         return json.dumps({'success': False}), 400, {'ContentType':'application/json'}
 
 
-@app.route("/login", methods=['GET'])
+@app.route("/login", methods=['GET','POST'])
 def login():
-    username = request.authorization['username']
-    password = request.authorization['password']
+    username = request.form['username']
+    password = request.form['password']
+
+    sessionId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(256))
+
+    response = flask.make_response()
+    response.status_code =302
+    response.set_cookie("sessID", sessionId,httponly=True, secure=True)
+    response.location = address + "account"
+    return  flask.redirect(flask.url_for('serve_account'))
 
     cur.execute("SELECT password FROM Users WHERE username = %s;", (username,))
     result = cur.fetchone()
     if result is not None and result[0] == password:
-        return json.dumps({'success': True}), 200, {'ContentType':'application/json'}
+        return response
+        #return json.dumps({'success': True, 'ssesId': ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(256))}), 200, {'ContentType':'application/json'}
     else:
         return json.dumps({'success': False}), 400, {'ContentType':'application/json'}
 
 
+
+'''
+@app.route("/setcookie", methods=['GET'])
+def setcookie():
+    sessionId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(256))
+    response = flask.make_response(render_template('login.html'))
+    response.status_code = 200
+    response.set_cookie("sessID", sessionId, httponly=True, secure=True)
+
+    return response
+'''
+
+
 @app.route("/films", methods=['GET'])
 def get_films():
-    username = request.authorization['username']
-    password = request.authorization['password']
+
+    print(request.cookies.get("sessID"))
+    #username = request.authorization['username']
+    #password = request.authorization['password']
+
+    username = request.json['username']
+    password = request.json['password']
 
     if authorize(username, password):
         cur.execute("""SELECT id, title, write, read, ownership FROM Films
@@ -81,7 +168,7 @@ def get_films():
 
         return json.dumps(dictionary_list)
 
-    return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
+    return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
 
 @app.route("/film/<int:id>", methods=['GET'])
 def get_film(id):
@@ -196,9 +283,22 @@ def set_permissions():
     return json.dumps({'success': False}), 400, {'ContentType':'application/json'}
 
 
+@app.route("/users", methods=['GET'])
+def get_users():
+
+    cur.execute("""SELECT username FROM Users""")
+    conn.commit()
+    users = cur.fetchall()
+    keys = ["username"]
+    dictionary_list = []
+    for user in users:
+        dictionary_list.append(dict(zip(keys, user)))
+
+    return json.dumps(dictionary_list)
+
 # TODO
 # transfer ownership
-# get users
+
 
 
 # database connection
@@ -216,7 +316,9 @@ except psycopg2.OperationalError:
 
 
 
-app.run()
+context = ('cert.pem', 'key.pem')#certificate and key files
+app.run(debug=True, ssl_context=context)
+
 
 cur.close()
 conn.close()
