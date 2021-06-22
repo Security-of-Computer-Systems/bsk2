f9b69094f6b0a7b7bf6286aa681625582f9e2542
Kits0n <kitson1021.99@gmail.com>
2021-06-22 16:21:08 +0200

    cosmetic changes

--- server.py
+++ server.py
@@ -103,46 +103,47 @@ def serve_film():
 
 @app.route('/edit-film', methods=['GET','PUT'])
 def serve_edit_film():
-    if request.method == "PUT":
-        sessionID = request.cookies.get("sessID")
-        id = request.json['id']
-        title = request.json['title']
-        time = request.json['time']
-
-        cur.execute("""
-                SELECT username 
-                FROM Users 
-                JOIN Permissions on Permissions.FK_User = Users.username
-                WHERE sessionID = %s and write = True and FK_Film = %s""", (sessionID, id,))
-
-        username = cur.fetchone()
-
-        if username is not None:
-
-            cur.execute("UPDATE Films SET title = %s, time = %s WHERE id = %s", (title, time, id,))
-            conn.commit()
-
-            return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
-        else:
-            return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
-    else:
+    if request.method == "GET":
         return flask.make_response(render_template('editFilm.html'))
+
+    # get parameters from request
+    sessionID = request.cookies.get("sessID")
+    id = request.json['id']
+    title = request.json['title']
+    time = request.json['time']
+
+    # find name of the user making the request and check if user has write permission to film with id <id>
+    cur.execute("""
+            SELECT username 
+            FROM Users 
+            JOIN Permissions on Permissions.FK_User = Users.username
+            WHERE sessionID = %s and write = True and FK_Film = %s""", (sessionID, id,))
+
+    username = cur.fetchone()
+    if username is not None:
+        # edit film with id <id>
+        cur.execute("UPDATE Films SET title = %s, time = %s WHERE id = %s", (title, time, id,))
+        conn.commit()
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    else:
+        return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
 
 @app.route('/delete-film', methods=["DELETE"])
 def serve_delete_film():
+    # get parameters from request
     sessionID = request.cookies.get("sessID")
     id = request.json['id']
 
+    # find name of the user making the request and check if user has write permission to film with id <id>
     cur.execute("""
                     SELECT username 
                     FROM Users 
                     JOIN Permissions on Permissions.FK_User = Users.username
                     WHERE sessionID = %s and write = True and FK_Film = %s""", (sessionID, id,))
-
     username = cur.fetchone()
 
     if username is not None:
-
+        # delete film with id <id>
         cur.execute("DELETE FROM Films WHERE id = %s;", (id,))
         conn.commit()
 
@@ -152,33 +153,33 @@ def serve_delete_film():
 
 @app.route('/add-film', methods=['GET','POST'])
 def serve_add_film():
-    if request.method == "POST":
 
-        sessionID = request.cookies.get("sessID")
-
-        cur.execute("""
-        SELECT username 
-        FROM Users 
-        WHERE sessionID = %s""",(sessionID,))
-
-        username = cur.fetchone()
-
-        if username is not None:
-            title = request.form.get("title")
-            time = request.form.get("time")
-
-            cur.execute("INSERT INTO Films  (title, time) VALUES(%s, %s)", (title, time,))
-            cur.execute("SELECT id FROM Films ORDER BY id DESC LIMIT 1")
-            cur.execute("INSERT INTO Permissions VALUES(%s,%s, %s, %s, %s, %s)",
-                        (cur.fetchone(),username, True, True, True, True))
-            conn.commit()
-
-            return flask.redirect(flask.url_for("serve_add_film"))
-        else:
-            return flask.redirect(flask.url_for("serve_add_film"))
-
-    else:
+    if request.method == "GET":
         return flask.make_response(render_template('addFilm.html'))
+
+    # get parameters from request
+    sessionID = request.cookies.get("sessID")
+
+    # find name of the user making the request
+    cur.execute("""
+    SELECT username 
+    FROM Users 
+    WHERE sessionID = %s""",(sessionID,))
+    username = cur.fetchone()
+
+    # Add film and permissions to database.
+    if username is not None:
+        title = request.form.get("title")
+        time = request.form.get("time")
+
+        cur.execute("INSERT INTO Films  (title, time) VALUES(%s, %s)", (title, time,))
+        cur.execute("SELECT id FROM Films ORDER BY id DESC LIMIT 1")
+        cur.execute("INSERT INTO Permissions VALUES(%s,%s, %s, %s, %s, %s)",
+                    (cur.fetchone(),username, True, True, True, True))
+        conn.commit()
+
+    return flask.redirect(flask.url_for("serve_add_film"))
+
 
 @app.route('/logout', methods=['POST'])
 def serve_logout():
@@ -196,47 +197,55 @@ def serve_logout():
 
 @app.route('/login', methods=['GET','POST'])
 def serve_login():
-    if request.method=="POST":
-        username = request.form.get("login")
-        password = request.form.get("password")
-
-        cur.execute("SELECT password FROM Users WHERE username = %s;", (username,))
-        result = cur.fetchone()
-        if result is not None and result[0] == password:
-            sessionId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(256))
-
-            cur.execute("""UPDATE Users 
-                        SET sessionID = %s
-                        WHERE username = %s;""", (sessionId,username,))
-
-            conn.commit()
-
-            response = flask.make_response()
-            response.status_code = 302
-            response.set_cookie("sessID", sessionId, httponly=True, secure=True)
-            response.location = address + "account"
-
-            return response
-
-        else:
-            return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
-
-    else:
+    if request.method=="GET":
         return flask.make_response(render_template('login.html'))
+
+    # get parameters from request
+    username = request.form.get("login")
+    password = request.form.get("password")
+
+    # find password hash in database of the user making the request
+    cur.execute("SELECT password FROM Users WHERE username = %s;", (username,))
+    result = cur.fetchone()
+
+    # compare password hashes
+    if result is not None and result[0] == password:
+
+        # create sessionID for user and put in database
+        sessionId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(256))
+
+        cur.execute("""UPDATE Users 
+                    SET sessionID = %s
+                    WHERE username = %s;""", (sessionId,username,))
+
+        conn.commit()
+
+        # send response
+        response = flask.make_response()
+        response.status_code = 302
+        response.set_cookie("sessID", sessionId, httponly=True, secure=True)
+        response.location = address + "account"
+
+        return response
+    else:
+        return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
+
 
 @app.route('/register', methods=['GET','POST'])
 def serve_register():
-    if request.method=='POST':
-        username = request.form.get("login")
-        password = request.form.get("password")
+    if request.method=='GET':
+        return flask.make_response(render_template('register.html'))
 
-        try:
-            cur.execute("INSERT INTO Users (username, password) VALUES(%s, %s)", (username, password,))
-            conn.commit()
-            return flask.redirect(flask.url_for("serve_login"))
-        except psycopg2.errors.UniqueViolation:
-            return flask.make_response(render_template('register.html'))
-    else:
+    # get parameters from request
+    username = request.form.get("login")
+    password = request.form.get("password")
+
+    # add user to database
+    try:
+        cur.execute("INSERT INTO Users (username, password) VALUES(%s, %s)", (username, password,))
+        conn.commit()
+        return flask.redirect(flask.url_for("serve_login"))
+    except psycopg2.errors.UniqueViolation:
         return flask.make_response(render_template('register.html'))
 
 @app.route('/account')
@@ -250,15 +259,18 @@ def serve_permissions():
 @app.route("/filmsWithPermissions", methods=['GET'])
 def get_films_with_permissions():
 
+    # get parameters from request
     sessionID = request.cookies.get("sessID")
 
+    # find all user's films and permissions for them
     cur.execute("""SELECT id, title, write, read, delegation, ownership FROM 
                 (SELECT FK_Film, write, read, ownership, delegation FROM Permissions
                 INNER JOIN Users ON Users.username = Permissions.FK_User
                 WHERE sessionID = %s) as user_permissions RIGHT JOIN Films on user_permissions.FK_Film = Films.id
                 """, (sessionID,))
-
     conn.commit()
+
+    # prepare json response
     films = cur.fetchall()
     keys = ["id","title", "write", "read","delegation", "ownership" ]
     dictionary_list = []
@@ -270,15 +282,18 @@ def get_films_with_permissions():
 @app.route("/films", methods=['GET'])
 def get_films():
 
+    # get parameters from request
     sessionID = request.cookies.get("sessID")
 
+    # find all user's films to which he has delegation or ownership permission
     cur.execute("""SELECT id,title FROM Permissions
                 INNER JOIN Users ON Users.username = Permissions.FK_User
                 INNER JOIN Films ON Films.id = Permissions.FK_Film
                 WHERE sessionID = %s and (delegation = True or ownership = True)
                 """, (sessionID, ))
-
     conn.commit()
+
+    # prepare json response
     films = cur.fetchall()
     keys = ["id","title"]
     dictionary_list = []
@@ -290,8 +305,10 @@ def get_films():
 @app.route("/film/<int:id>", methods=['GET'])
 def get_film(id):
 
+    # get parameters from request
     sessionID = request.cookies.get("sessID")
 
+    # check if user has read permission to film with id <id>
     cur.execute("""SELECT read FROM Permissions 
     INNER JOIN Films ON Films.id = Permissions.FK_Film
     INNER JOIN Users ON Users.username = Permissions.FK_User
@@ -299,13 +316,13 @@ def get_film(id):
     """, (id, sessionID))
     result = cur.fetchone()
     if result is not None and (result[0] == True):
+        # find film with id <id>
         cur.execute("SELECT * FROM Films WHERE id = %s;", (id,))
 
+        # prepare json response
         film = cur.fetchall()[0]
         keys = ["id", "title", "time"]
-
         return json.dumps(dict(zip(keys, film)))
-
     return flask.redirect(flask.url_for("serve_films"))
 
 @app.route("/filmToEdit/<int:id>", methods=['GET'])
@@ -347,6 +364,7 @@ def can_delegate(edge, delegation, read, write):
         # check if graph has cycles
         for cycle in nx.simple_cycles(tmp_graph):
             return False
+        # check if node has more than 1 predecessors
         if len([n for n in tmp_graph.predecessors(edge[1])]) >= 2:
             return False
     if read:
@@ -355,6 +373,7 @@ def can_delegate(edge, delegation, read, write):
         # check if graph has cycles
         for cycle in nx.simple_cycles(tmp_graph):
             return False
+        # check if node has more than 1 predecessors
         if len([n for n in tmp_graph.predecessors(edge[1])]) >= 2:
             return False
     if write:
@@ -363,6 +382,7 @@ def can_delegate(edge, delegation, read, write):
         # check if graph has cycles
         for cycle in nx.simple_cycles(tmp_graph):
             return False
+        # check if node has more than 1 predecessors
         if len([n for n in tmp_graph.predecessors(edge[1])]) >= 2:
             return False
 
@@ -376,6 +396,8 @@ def can_delegate(edge, delegation, read, write):
 
 @app.route("/permissions", methods=['POST'])
 def set_permissions():
+
+    # get parameters from request
     sessionID = request.cookies.get("sessID")
     id = request.json['id']
     username2 = request.json['username2']
@@ -383,13 +405,13 @@ def set_permissions():
     write = request.json['write']
     delegation = request.json['delegation']
 
-    # find donor user's username
+    # find donor's username
     cur.execute("""SELECT username FROM Users  
         WHERE sessionID = %s
         """, (sessionID,))
     donor_username = cur.fetchone()[0]
 
-
+    # find donor permissions to film with id <id>
     cur.execute("""SELECT delegation, read, write FROM Permissions 
     INNER JOIN Films ON Films.id = Permissions.FK_Film
     INNER JOIN Users ON Users.username = Permissions.FK_User
@@ -397,13 +419,13 @@ def set_permissions():
     """, (id, sessionID))
     result = cur.fetchone()
 
-    #  Check if user has delegation permission, assigned permissions,
+    #  Check if user has delegation permission and   permissions he wants to transfer
     if result is not None and result[0] == True and (read <= result[1]) and (write <= result[2]):
         # Elimination of cycles in the permission delegation graph
         # Disability of granting permission to the same object by more than one donor
         if can_delegate((donor_username, username2), delegation, read, write):
 
-            # Sprawdź czy relacja użytkownika z filmem istnieje
+            # Check if there is relation between user and film
             cur.execute("""SELECT * FROM Permissions 
                     WHERE FK_Film = %s and FK_User = %s
                     """, (id, username2))
@@ -412,7 +434,6 @@ def set_permissions():
                 # Insert
                 cur.execute("INSERT INTO Permissions VALUES(%s,%s, %s, %s, %s, %s)",
                             (id, username2, read, write, False, delegation,))
-
             else:
                 # Update
                 cur.execute("UPDATE Permissions SET read = %s, write = %s, delegation = %s WHERE FK_Film = %s and FK_User = %s", (read, write, delegation, id, username2))
@@ -424,8 +445,11 @@ def set_permissions():
 @app.route("/users", methods=['GET'])
 def get_users():
 
+    # find all users
     cur.execute("""SELECT username FROM Users""")
     conn.commit()
+
+    # prepare json response
     users = cur.fetchall()
     keys = ["username"]
     dictionary_list = []
@@ -436,6 +460,8 @@ def get_users():
 
 @app.route("/transfer", methods=['POST'])
 def transfer_ownership():
+
+    # get parameters from request
     id = request.json['id']
     username2 = request.json['username2']
     read = request.json['read']
@@ -463,7 +489,7 @@ def transfer_ownership():
             # Update
             cur.execute("UPDATE Permissions SET read = %s, write = %s, delegation = %s, ownership = %s WHERE FK_Film = %s and FK_User = %s",
                         (read, write, delegation, True, id, username2))
-        # removing permissions of donor
+        # removing all permissions of donor
         cur.execute("UPDATE Permissions SET read = %s, write = %s, delegation = %s, ownership = %s WHERE FK_Film = %s and FK_User = %s",
                     (False, False, False, False, id, result[4]))
         conn.commit()
